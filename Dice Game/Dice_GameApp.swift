e5eee380bc2aa8a76b258dfb9cf8039905da9791
michelle379 <michelle379123@gmail.com>
2024-03-15 15:36:06 +0800

    Initial Commit

---
+++ Dice Game/Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_GameApp.swift
+//  Dice Game
+//
+//  Created by Apple on 2024/3/15.
+//
+
+import SwiftUI
+
+@main
+struct Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
